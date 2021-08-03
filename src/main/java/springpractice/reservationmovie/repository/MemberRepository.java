1cad6264ae43fe03331c5bd7187e4a6fddd59059
ldk <ldk980130@gmail.com>
2021-08-04 00:57:15 +0900

    feat: MemberRepository 추가

---
+++ src/main/java/springpractice/reservationmovie/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package springpractice.reservationmovie.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import springpractice.reservationmovie.domain.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public Member findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return member;
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
